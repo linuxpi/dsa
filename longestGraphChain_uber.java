7cb65a66fa8b8b81889e9db88cee30a3a63ae9ab
linuxpi <varunb94@gmail.com>
2022-09-17 16:56:28 +0530

    add question

--- longestGraphChain_uber.java
+++ longestGraphChain_uber.java
@@ -1,3 +1,46 @@
+/*
+Given an input of a list of lists, each list contains a list of node_ids. The order of the list [A,B] represents a one way connection from node A to node B, and node_ids that are the same between different lists are the same node.
+
+We want to find the number of nodes in the longest chain across all the input lists.
+
+  
+
+Lets say we are given list -> ["A", "C", "D"], then A connected to C, and C connected to D. This way we will be given list of lists which has several node links, we need to longest connected chain in this.
+
+  
+
+Input:
+
+[["A", "C", "D"],
+
+["A", "B", "E"],
+
+["D", "F", "G", "H"],
+
+["Z", "X", "C", "Q", "R"]]
+
+  
+
+R -> [Q]
+
+Q -> [C]
+
+C -> [A, X]
+
+A -> [F, G, H]
+
+X -> [T, U, V]
+
+  
+
+Solution for longest chain - z, x, c, d, f, g, h
+
+Output: 7
+
+  
+  
+
+*/
 
 import java.util.*;
 
